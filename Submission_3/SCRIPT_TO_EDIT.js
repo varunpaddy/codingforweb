81038c12d3e1b590c0ab41dcbd634d697844f451
varunpaddy <141609860+varunpaddy@users.noreply.github.com>
2023-08-21 11:08:34 +0530

    Add files via upload

---
+++ Submission_3/SCRIPT_TO_EDIT.js
@@ -0,0 +1,15 @@
+let div2 = document.getElementById("box");
+let btn = document.getElementById("button");
+
+function myFunction() {
+    if (div2.style.display === "none"){
+        div2.style.display = "block";
+    } 
+    else {
+        div2.style.display = "none";
+    }
+    
+}
+
+
+
