ed602b160dc4532dce0349ca8ebde66796a064a1
varunpaddy <141609860+varunpaddy@users.noreply.github.com>
2023-08-21 11:07:19 +0530

    Add files via upload

---
+++ Submission_1/script.js
@@ -0,0 +1,10 @@
+function changeBodyColor(){
+    let body = document.getElementById('body');
+
+    if (body.style.backgroundColor === 'bisque')
+    {
+        body.style.backgroundColor = 'red';
+    } 
+    
+    else { body.style.backgroundColor = 'bisque';}
+}
